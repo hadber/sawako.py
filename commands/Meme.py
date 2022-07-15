88107b7b56d6f758dc577b047276b69bc15a8ea2
hadber <adber@tuta.io>
2022-07-16 01:01:46 +0200

    bottom text

--- commands/Meme.py
+++ commands/Meme.py
@@ -40,4 +40,25 @@ class Meme(Command):
 
     def memeify(self, name, bot, top):
         im = Image.open(name)
-        
+        draw = ImageDraw.Draw(im)
+        start_size = 32
+        font = ImageFont.truetype("fonts/CooperHewitt-Bold.otf", start_size)
+        font_size = font.getsize(bot)
+        width, height = im.size
+
+        current_use = font_size[0] / width
+        newp = (start_size * 0.9) / current_use
+        new_size = int(newp)
+        new_font = font.font_variant(size=new_size)
+
+        if new_size < 25:
+            stroke_size = 1
+        elif new_size < 50:
+            stroke_size = 2
+        elif new_size < 75:
+            stroke_size = 3
+        else:
+            stroke_size = 5
+        draw.text((width*0.05, height*0.05), bot, "#fff", font=new_font, stroke_width=stroke_size, stroke_fill="#000")
+
+        im.save(f"{name}_meme.png")
